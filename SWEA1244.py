9560556ab253d68b4acabb03226ef0eff1524593
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-08-02 17:49:50 +0900

    solve

--- SWEA1244.py
+++ SWEA1244.py
@@ -1,6 +1,5 @@
-# SWEA 1244 최대 상금 -> 15개 테스트 케이스 중 1개 통과 못함
-# 번 테스트 케이스 다시 생각해야함 -> 아래 코드로는 88823이 나옴
-# 교환가능한 숫자들과 인덱스의 리스트를 만들어서 조합하는 식으로 해보자!
+# SWEA 1244 최대 상금
+# 단순화 시켜볼 것
 
 T = int(input())
 
@@ -11,34 +10,50 @@ for test in range(1, T+1):
 
     if num_list.count(max(num_list)) == len(num_list): # 자릿수가 모두 같으면, 바꾸는게 무의미함
         change_cnt = cnt
-
     else:
         change_cnt = 0
 
+    sorted_list = sorted(num_list) # 오름차순
+    #print(sorted_list)
     for i in range(len(num_list)):
-        if change_cnt >= cnt:
+        if change_cnt == cnt or num_list == sorted_list[::-1]:
             break
 
-        max_idx = len(num_list) - num_list[::-1].index(max(num_list[i:])) - 1 # 최댓값이 있는 인덱스를 찾는데, 여러개 있으면 가장 오른쪽의 인덱스를 찾음
-        if i == max_idx:
-            continue
+        if num_list[i:].count(max(num_list[i:])) == 1:
+            max_idx = num_list[i:].index(max(num_list[i:])) + i
+            if max_idx == i:
+                continue
+            else:
+                num_list[i], num_list[max_idx] = num_list[max_idx], num_list[i]
+                change_cnt += 1
+                #print('a', change_cnt, num_list) # 어디에서 자릿수가 바뀌는지 확인하기 위함
+
         else:
-            num_list[i], num_list[max_idx] = num_list[max_idx], num_list[i]
-            change_cnt += 1
+            max_idx_list = [] # max index 저장하는 list
+            for j in range(len(num_list)):
+                if num_list[j] == max(num_list[i:]):
+                    max_idx_list.append(j)
 
-    if change_cnt >= cnt:
+            sorted_idx = sorted_list.index(num_list[i]) # 바꾸고자하는 수가 뒤에서 몇 번째로 큰 수인지 체크
+
+            if sorted_idx+1 <= len(max_idx_list):
+                max_idx = max_idx_list[::-1][sorted_idx] # max 숫자가 있는 인덱스들 중 바꾸고자하는 수의 순서에 해당하는 max index 찾기
+                num_list[i], num_list[max_idx] = num_list[max_idx], num_list[i]
+                change_cnt += 1
+                #print('b', change_cnt, num_list)
+            else:
+                max_idx = max_idx_list[0]
+                num_list[i], num_list[max_idx] = num_list[max_idx], num_list[i]
+                change_cnt += 1
+                #print('c', change_cnt, num_list)
+
+    if (cnt - change_cnt) % 2 == 0: # 짝수면 왔다갔다 했다 치고 답은 변하지 않음
         pass
-
-    elif (cnt - change_cnt) % 2 == 0: # 짝수면 왔다갔다 했다 치고 답은 변하지 않음
-        pass
-
     elif len(set(num_list)) < len(num_list): # num_list 내부에 같은 수가 있으면 그 안에서 서로 왔다갔다하면 됨
         pass
-
-    elif (cnt - change_cnt) % 2 == 1: # cnt - change_cnt 갸 홀수면 왔다갔다하고 한번이 남는다. 그때는 가장 뒤의 숫자 두개를 바꾸는게 영향이 가장 적음
+    elif (cnt - change_cnt) % 2 == 1: # cnt - change_cnt 가 홀수면 왔다갔다하고 한번이 남는다. 그때는 가장 뒤의 숫자 두개를 바꾸는게 영향이 가장 적음
         num_list[-1], num_list[-2] = num_list[-2], num_list[-1]
 
     max_number = int(''.join(list(map(str, num_list))))
 
     print(f'#{test} {max_number}')
-
