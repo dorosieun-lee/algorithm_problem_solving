630d62a6b17bde2885ebcfaad2a27ca660bcc12c
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-08-03 15:39:11 +0900

    solve

--- SWEA1210.py
+++ SWEA1210.py
@@ -12,31 +12,31 @@ for test in range(1, T+1):
 
     while i > 0:
         if dir[0] == 1: # 올라가는 중이었다
-            if j != 0 and ladder[i][j - 1] == 1:
+            if j != 0 and ladder[i][j - 1] == 1: # 왼쪽 갈 수 있나?
                 dir[1] = 1
                 dir[0] = 0
                 j -= 1
-            elif j != 99 and ladder[i][j+1] == 1:
+            elif j != 99 and ladder[i][j+1] == 1: # 오른쪽 갈 수 있나?
                 dir[2] = 1
                 dir[0] = 0
                 j += 1
-            elif ladder[i-1][j] == 1:
+            else: # 안되면 그냥 가던 길 계속 가
                 i -= 1
 
         if dir[1] == 1: # 왼쪽으로 가는 중이었다
-            if ladder[i - 1][j] == 1:
+            if ladder[i - 1][j] == 1: # 올라갈 수 있나?
                 dir[0] = 1
                 dir[1] = 0
                 i -= 1
-            else:
+            else: # 안되면 그냥 가던 길 계속 가
                 j -= 1
 
         if dir[2] == 1: # 오른쪽으로 가는 중이었다
-            if ladder[i - 1][j] == 1:
+            if ladder[i - 1][j] == 1: # 올라갈 수 있나?
                 dir[0] = 1
                 dir[2] = 0
                 i -= 1
-            else:
+            else: # 안되면 그냥 가던 길 계속 가
                 j += 1
 
 
