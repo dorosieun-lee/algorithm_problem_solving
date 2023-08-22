b4819441b5dd736997e9ae37deef5e6183fed651
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-08-22 23:14:45 +0900

    solve

--- 프로그래머스_쿼드압축후개수세기ing.py
+++ 프로그래머스_쿼드압축후개수세기ing.py
@@ -1,25 +1,31 @@
 # 프로그래머스 쿼드압축 후 개수 세기
+import itertools
 
-def compress(arr):
-    if set(arr) == 1:
+def compress(arr, zero_cnt, one_cnt):
+    print(list(itertools.chain(*arr)))
+    if len(set(list(itertools.chain(*arr)))) == 1:
         if arr[0] == 1:
             zero_cnt += 1
+            return
         else:
             one_cnt += 1
+            return
 
     N = len(arr)
-    areas = [[0, 0, N//2 - 1, N//2 - 1], [N//2 - 1, 0, N//2, N-1], [N//2, 0, N//2 - 1, N-1], [N//2, N//2, N-1, N-1]]
+    areas = [[0, 0, N//2-1, N//2-1], [0, N//2, N//2-1, N-1], [N//2, 0, N-1, N//2-1], [N//2, N//2, N-1, N-1]]
     for area in areas:
         sub_arr = []
-        for i in range(area[0], area[1]+1):
-            for
-        compress()
-def solution(arr):
-    zero_cnt = 0
-    one_cnt = 0
-    compress(arr)
+        for row in range(area[0], area[1]+1):
+            sub_arr.append(arr[row][area[2]:area[3]+1])
+        compress(sub_arr, zero_cnt, one_cnt)
 
-    answer = []
+
+def solution(arr):
+    zero_cnt, one_cnt = 0, 0
+    compress(arr, zero_cnt, one_cnt)
+
+    answer = [zero_cnt, one_cnt]
     return answer
 
 arr = [[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]
+print(solution(arr))
--- 프로그래머스_타겟넘버.py
+++ 프로그래머스_타겟넘버.py
@@ -31,3 +31,28 @@ target = 4
 numbers = [1, 1, 1, 1, 1]
 target = 3
 print(solution(numbers, target))
+
+'''
+신박하고 깔끔한 풀이!
+재귀함수 이용
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+반복문으로 처리
+def solution(numbers, target):
+    q = [0]
+    for n in numbers:
+        s = []
+        for _ in range(len(q)):
+            x = q.pop()
+            s.append(x + n)
+            s.append(x + n*(-1))
+        q = s.copy()
+    return q.count(target)
+
+'''
