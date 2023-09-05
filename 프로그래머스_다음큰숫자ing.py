0abd1ba3a66a7c1d6713aad317957ca43066ec87
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-09-05 17:57:21 +0900

    solving

--- 프로그래머스_다음큰숫자ing.py
+++ 프로그래머스_다음큰숫자ing.py
@@ -4,33 +4,33 @@
 # 1의 개수가 같은 이진수 가능한거 다 만들어서
 # n보다 크면서 가장 작은거 만들기?
 
+MIN = float("inf")
+
+def make_binary_list(i, n, bin_str, length, org):
+    global MIN
+    if i == n or len(bin_str) == length:
+        if i == n:
+            bin_num = bin_str + '0' * (length - len(bin_str))
+            num = int(bin_num, 2)
+            print(num)
+            if num > org:
+                MIN = min(MIN, num)  # MIN이 안바뀜!!
+        return
+
+    make_binary_list(i + 1, n, bin_str + '1', length, org)
+    make_binary_list(i, n, bin_str + '0', length, org)
 
 
 def solution(n):
-    def make_binary_list(i, n, bin_str, length):
-        if i == n or len(bin_str) == length:
-            if i == n:
-                bin_list.append(bin_str + '0' * (length - len(bin_str)))
-            return
-
-        make_binary_list(i + 1, n, bin_str + '1', length)
-        make_binary_list(i, n, bin_str + '0', length)
-
+    global MIN
     n_bin = bin(n)
     cnt_one = n_bin.count('1')
     length = len(n_bin) - 2
     if cnt_one == length:
         length += 1
-    bin_list = []
-    make_binary_list(0, cnt_one-1, '1', length)
-    # print(bin_list)
-    answer = float("inf")
-    for b in bin_list:
-        num = int(b, 2) # 이진수를 십진수로 바꿈
-        if n < num:
-            answer = min(num, answer)
+    make_binary_list(0, cnt_one-1, '1', length, n)
 
-    return answer
+    return MIN
 
 n = 15
 print(solution(n))
