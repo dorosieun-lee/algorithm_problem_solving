84e2406d56840521d87d9fcc80e17cacc9827517
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-09-13 17:04:41 +0900

    solve

--- Baekjoon11052.py
+++ Baekjoon11052.py
@@ -2,6 +2,7 @@
 
 # 카드 N개를 구매하기 위해 지불해야하는 금액의 최댓값을 구해라
 # 구매한 카드팩에 포함되어 있는 카드 개수의 합은 N과 같다.
+"""
 import sys
 
 def combination(cards, s, n):
@@ -31,6 +32,19 @@ total_price = []
 combination(0, 0, N)
 
 print(max(total_price))
+"""
 
 # DP로 푼다면?!
 # 2부터, 자신이 가질 수 있는 최댓값을 저장해서 N까지 올라감 -> 구해야하는 경우의 수가 줄어듬
+N = int(input())
+prices = [0] + list(map(int, input().split()))
+
+for i in range(2, N+1):
+    MAX = 0
+    for j in range(0, i//2+1):
+        #print(j, i-j)
+        MAX = max(prices[j] + prices[i-j], MAX)
+
+    prices[i] = MAX
+
+print(prices[-1])
