8b95104e9798b117a8974f20c1d62dd7c4051e61
dorosieunLee <dorothy.lee.723@gmail.com>
2023-10-10 14:10:58 +0900

    solve

---
+++ 프로그래머스_롤케이크.py
@@ -0,0 +1,25 @@
+# 프로그래머스 롤케이크
+
+def solution(topping):
+    left = set()
+    right = {}
+    answer = 0
+
+    for i in topping:
+        right[str(i)] = right.get(str(i), 0)
+        right[str(i)] += 1
+
+    for i in topping:
+        left.add(i)
+        right[str(i)] -= 1
+        if right[str(i)] == 0:
+            del right[str(i)]
+
+        if len(left) == len(right.keys()):
+            answer += 1
+
+    return answer
+
+topping = [1, 2, 1, 3, 1, 4, 1, 2]
+# topping = [1,2,3,1,4]
+print(solution(topping))
