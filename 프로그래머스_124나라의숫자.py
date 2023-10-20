c313ca62212809d8f4359c49fc82cd281a0bcc92
dorosieun-lee <dorothy.lee.723@gmail.com>
2023-10-20 09:12:01 +0900

    solve

---
+++ 프로그래머스_124나라의숫자.py
@@ -0,0 +1,25 @@
+# 프로그래머스 124 나라의 숫자
+
+def solution(n):
+	my_dict = {1: '1', 2: '2', 3: '4'}
+	if n in my_dict.keys():
+		return my_dict[n]
+
+	answer = ''
+	main = n
+	while main > 0:
+		remain = main % 3
+		if remain == 0:
+			answer = my_dict[3] + answer
+			main = main - 3
+		else:
+			answer = my_dict[remain] + answer
+			main = main - remain # 3의 배수
+
+		main = main//3 # 3으로 나눈 몫
+
+	return answer
+
+
+for n in range(0, 25):
+	print(n,': ',solution(n))
