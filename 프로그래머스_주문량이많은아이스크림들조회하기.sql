dc3ab926ca2214a798e44a6ed1dc21d467ce47ba
dorosieun-lee <dorothy.lee.723@gmail.com>
2024-03-23 14:25:49 +0900

    solve

--- 프로그래머스_주문량이많은아이스크림들조회하기.sql
+++ 프로그래머스_주문량이많은아이스크림들조회하기.sql
@@ -1,3 +1,4 @@
+-- inner join 이용 -> 가로로 합침
 SELECT FLAVOR FROM 
 (SELECT F.FLAVOR, F.TOTAL_ORDER + J.TOTAL_ORDER AS TOTAL_ORDER 
 FROM FIRST_HALF F
@@ -7,3 +8,13 @@ INNER JOIN (
 ) J ON J.FLAVOR = F.FLAVOR
 ORDER BY TOTAL_ORDER DESC) NEW_TABLE
 LIMIT 3;
+
+-- union all 이용 -> 중복을 허용하면서 세로로 합침
+select flavor from (
+select flavor, total_order from first_half
+union all
+select flavor, total_order from july
+) new_table
+group by flavor
+order by sum(total_order) desc
+limit 3;
